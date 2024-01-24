ea14ee5e843042917b278cc5b7554d80be1e6285
cristinagiacobbe <crigiak@gmail.com>
2024-01-24 22:28:14 +0100

    Print todolist on page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <script>
 export default {
+  name: "App",
+
   data() {
     return {
-
+      todolist: [
+        {
+          text: "Ascoltare la registrazione Boolean del 23/01/24",
+          done: true,
+        },
+        {
+          text: "Fare esercizio Boolean del 23/01/24",
+          done: false,
+        },
+        {
+          text: "Fare il bucato",
+          done: false,
+        },
+        {
+          text: "Archiviare ricevute",
+          done: false,
+        },
+        {
+          text: "Fare esercizio di calligrafia",
+          done: true,
+        }
+      ]
     }
   }
 }
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <h1>My todolist</h1>
+  <ul>
+    <li v-for="action in todolist">
+      {{ action.text }}
+    </li>
+  </ul>
 </template>
 
 <style></style>
